801a65b8e410d0b630c54e7d15fba46fe0f9781e
Aravinda24 <122682253+Aravinda24@users.noreply.github.com>
2023-10-10 13:28:21 +0530

    Time: 729 ms (27.38%), Space: 68.8 MB (70.25%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.java
+++ 0815-bus-routes/0815-bus-routes.java
@@ -1,48 +1,56 @@
 class Solution {
     public int numBusesToDestination(int[][] routes, int source, int target) {
-        
-       if (source == target) return 0;
- 
-        Map<Integer, List<Integer>> stopToRoutes = new HashMap<>();
+     if(source == target)return 0;
 
-        Set<Integer> visitedRoutes = new HashSet<>();
+		HashMap<Integer, List<Integer>> hashMap = new HashMap();
 
-        Queue<Integer> queue = new LinkedList<>();
+		for (int i = 0; i < routes.length; i++) {
 
-        queue.offer(source);
+			for (int j = 0; j < routes[i].length; j++) {
 
-        int busesTaken = 0;
+				if (hashMap.containsKey(routes[i][j])) {
+					List<Integer> buses = hashMap.get(routes[i][j]);
+					buses.add(i);
+					hashMap.put(routes[i][j], buses);
+				} else {
+					List<Integer> buses = new ArrayList <Integer>();
+					buses.add(i);
+					hashMap.put(routes[i][j], buses);
+				}
+				
 
-        for (int i = 0; i < routes.length; i++) {
-            for (int stop : routes[i]) {
-                stopToRoutes.computeIfAbsent(stop, k -> new ArrayList<>()).add(i);
-            }
-        }
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            busesTaken++;
-            
-            for (int i = 0; i < size; i++) {
-                int currentStop = queue.poll();
-                List<Integer> routesForCurrentStop = stopToRoutes.get(currentStop);
-                
-                if (routesForCurrentStop == null) continue;
-                
-                for (int route : routesForCurrentStop) {
-                    if (visitedRoutes.contains(route)) continue;
-                    
-                    visitedRoutes.add(route);
-                    
-                    for (int nextStop : routes[route]) {
-                        if (nextStop == target) return busesTaken;
-                        
-                        queue.offer(nextStop);
-                    }
-                }
-            }
-        }
-        
-        return -1; 
+			}
+		}
+
+		int noOfBusTaken = 0;
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(source);
+		HashSet<Integer> checked = new HashSet<Integer>();
+
+		 while (!q.isEmpty()) {
+	            int size = q.size();
+	            noOfBusTaken++;
+	            
+	            for (int i = 0; i < size; i++) {
+	                int currentStop = q.poll();
+	                List<Integer> routesForCurrentStop = hashMap.get(currentStop);
+	                
+	                if (routesForCurrentStop == null) continue;
+	                
+	                for (int route : routesForCurrentStop) {
+	                    if (checked.contains(route)) continue;
+	                    
+	                    checked.add(route);
+	                    
+	                    for (int nextStop : routes[route]) {
+	                        if (nextStop == target) return noOfBusTaken;
+	                        
+	                        q.offer(nextStop);
+	                    }
+	                }
+	            }
+	        }
+	        
+	        return -1; 
     }
 }
